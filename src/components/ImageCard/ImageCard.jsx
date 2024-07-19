ecf93fbcb82bcdae9104224f1023893c74417500
Vikasa2704 <vikasa2704@gmail.com>
2024-07-19 09:00:13 +0300

    add onClick in img

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,17 +1,17 @@
 import css from './ImageCard.module.css';
 
-
-const ImageCard = ({alt_description, urls, updateModalStateData}) => {
+const ImageCard = ({ alt_description, urls, updateModalStateData }) => {
     return (
-        <div className={css.cardWrapper} onClick={() => updateModalStateData(urls.regular, alt_description)}
-		>
-            <img className = {css.ImageCard}
-            src={urls.small} 
-            alt={alt_description} />
+        <div className={css.cardWrapper}>
+            <img
+                className={css.ImageCard}
+                src={urls.small}
+                alt={alt_description}
+                onClick={() => updateModalStateData(urls.regular, alt_description)}
+            />
             <p className={css.cardDescription}>{alt_description}</p>
         </div>
-
     );
 };
 
-export default ImageCard
+export default ImageCard;
